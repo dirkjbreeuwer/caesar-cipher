7f2cb307124d51af62aedd7b51ef76cf9abfc432
Dirk Breeuwer <87372550+dirkjbreeuwer@users.noreply.github.com>
2023-10-29 08:11:45 +0000

    Add functionality to try all shifts

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
-# caesar-cipher
-A simple Caesar cipher solver
+# Caesar Cipher Streamlit App
+
+This Streamlit app allows users to cipher and decipher text using Caesar's algorithm.
+
+## How to use
+
+1. Input the text you want to cipher or decipher.
+2. Choose whether you want to cipher or decipher the text.
+3. Input the shift value (the number of positions each letter of the text should be shifted).
+4. Click the "Submit" button to see the result.
+
+If you want to decrypt a text but don't know the shift value, you can:
+
+1. Input a small subset of the text you want to decrypt.
+2. Click the "Start" button to see the decrypted text for all possible shift values.
+
+## How to run locally
+
+1. Clone this repository to your local machine.
+2. Install Streamlit:
+
+`pip install streamlit`
+
+3. Run the app:
+
+`streamlit run app.py`
+
+## How to deploy
+
+You can deploy this app to Streamlit sharing by following these steps:
+
+1. Create a Streamlit sharing account and log in.
+2. Click on "New app" and follow the instructions to deploy your app.
--- app.py
+++ app.py
@@ -1,32 +1,64 @@
+"""
+This Streamlit app provides a simple interface for encrypting and decrypting text using the Caesar Cipher algorithm.
+"""
+
 import streamlit as st
 
-def caesar_cipher(text, shift, direction):
-    result = ""
-    for i in range(len(text)):
-        char = text[i]
+def caesar_cipher(input_text, shift_val, direction_val="encrypt"):
+    """
+    Apply the Caesar Cipher algorithm to a given text.
+
+    Args:
+    input_text (str): The text to be encrypted or decrypted.
+    shift_val (int): The shift value to be used in the Caesar Cipher algorithm.
+    direction_val (int): The direction of the shift. Use 1 for encryption and -1 for decryption.
+
+    Returns:
+    str: The encrypted or decrypted text.
+    """
+    result_text = ""
+    for char in input_text:
         if char.isalpha():
             base = ord('A') if char.isupper() else ord('a')
-            result += chr((ord(char) - base + shift * direction) % 26 + base)
+            result_text += chr((ord(char) - base + shift_val * direction_val) % 26 + base)
         else:
-            result += char
-    return result
+            result_text += char
+    return result_text
 
-def main():
-    st.title("Caesar Cipher App")
-    text = st.text_area("Enter your text:")
-    shift = st.slider("Select the shift value:", 1, 25, 3)
-    direction = st.radio("Select the operation:", ["Encrypt", "Decrypt"])
+def try_all_shifts(input_text):
+    """
+    Try all possible shift values to decrypt a given text using the Caesar Cipher algorithm.
 
-    if st.button("Submit"):
-        if text:
-            if direction == "Encrypt":
-                result = caesar_cipher(text, shift, 1)
-                st.success(f"Encrypted text: {result}")
-            else:
-                result = caesar_cipher(text, shift, -1)
-                st.success(f"Decrypted text: {result}")
-        else:
-            st.error("Please enter some text.")
+    Args:
+    input_text (str): The text to be decrypted.
 
-if __name__ == "__main__":
-    main()
+    Returns:
+    list of tuple: A list of tuples where each tuple contains a shift value and the corresponding decrypted text.
+    """
+    results = []
+    for shift in range(26):
+        decrypted_text = caesar_cipher(input_text, shift, direction_val=-1)
+        results.append((shift, decrypted_text))
+    return results
+
+st.title("Caesar Cipher App")
+
+input_text = st.text_area("Enter text:")
+input_shift = st.number_input("Enter shift value:", min_value=0, max_value=25, value=0)
+direction = st.radio("Choose operation:", ["Encrypt", "Decrypt"])
+
+if direction == "Encrypt":
+    encrypted_text = caesar_cipher(input_text, input_shift, direction_val=1)
+    st.write("Encrypted text:", encrypted_text)
+else:
+    decrypted_text = caesar_cipher(input_text, input_shift, direction_val=-1)
+    st.write("Decrypted text:", decrypted_text)
+
+st.header("Want to decrypt a text but don't know the shift value?")
+
+subset_text = st.text_area("Enter a small subset of text:")
+start_button = st.button("Start", disabled=not subset_text)
+if start_button:
+    all_shifts_results = try_all_shifts(subset_text)
+    for shift_val, output_text in all_shifts_results:
+        st.write(f"Shift {shift_val}: {output_text}")
